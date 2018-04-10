3e7a1eb1dbd08f5d9f4051119ce390650ab2b395
htectl <htectl@mailinator.com>
2018-04-10 11:45:04 -0400

    upgraded NTL to work with BigchainDB 2.0
    latest release of BigchainDB had several API changes, now reflected in the new NTL and NTL_Tester files

--- ntl.py
+++ ntl.py
@@ -15,25 +15,94 @@ from bigchaindb_driver.crypto import generate_keypair as gen
 import requests
 
 class NetworkTransportLayer():
-	def __init__(self, app_key, options=None):
+	def __init__(self, app_id, app_key, options=None):
 		self.tokens={
-			'app_id' : '81338ad5',
+			'app_id' : app_id,
 			'app_key' : app_key
 		}
 
-		self.chain = bdb('https://test.bigchaindb.com', 
+		self.chain = bdb('https://test2.bigchaindb.com', 
 			headers=self.tokens)
 		self.options = options
 
-		self.log = {'made_order_li' : [], # list of created orders
-					'sign_order_li' : [], # list of signed orders
-					'sent_order_dt' : {}} #
+		self.log = {'u_ord_li' : [], # list of created orders
+					's_ord_li' : [], # list of signed orders
+					'b_ord_dt' : {}} # dictionary of broadcasted orders,
+									 # with key TXID
 
-	def make_singlesign_order(self, order, metadata, pub_key):
-		made_tx = self.chain.transactions.prepare(
+	def make_singlesign_order(self, raw_order, metadata, public_key):
+		'''
+		This method creates a json order ready to be signed and
+		broadcast to a BigchainDB cluster. It takes a PUBLIC KEY and
+		an ORDER dictionary, as well as relevant METADATA.
+
+		This method is for a single-signature order.
+		'''
+
+		new_order = self.chain.transactions.prepare(
 			operation = 'CREATE',
-			signers = pub_key,
-			asset = order,
+			signers = public_key,
+			asset = raw_order,
 			metadata = metadata)
 
-		self.log[]
+		self.log['u_ord_li'].append(new_order)
+		return new_order
+
+	def sign_order(self, order, private_key):
+		'''
+		This method takes an ORDER created with make_singlesign_order()
+		or make_order(), along with a PRIVATE KEY, and returns 
+		a signed order, ready to be broadcast to BigchainDB.
+		'''
+
+		signed_order = self.chain.transactions.fulfill(
+			order, private_keys=private_key)
+
+		self.log['s_ord_li'].append(signed_order)
+		return signed_order
+
+	def send_order(self, signed_order):
+		'''
+		This method pushes ONE signed order to BigchainDB. 
+		It takes a single SIGNED_ORDER as an argument, and returns
+		a tuple with the TX_ID and the full transaction.
+		'''
+
+		sent_order = self.chain.transactions.send(signed_order)
+
+		self.log['b_ord_dt'][sent_order['id']] = sent_order
+		return (sent_order['id'], sent_order)
+
+	def get_order_height(self, tx_id):
+		'''
+		Returns the block height of the block the order with
+		the specified TX_ID was validated in.
+		'''
+
+		return self.chain.blocks.get(txid=tx_id)
+
+class KeypairGenerator():
+	'''
+	Only use this class to generate keypairs for 
+	TESTING PURPOSES!!!
+
+	For real orders, actual public and private keypairs
+	should be used for preparing and signing all 
+	transactions.
+	'''
+
+	def __init__(self):
+		self.pair = gen()
+
+	def get_public_key(self):
+		'''
+		Returns public key (addresss)
+		'''
+		return self.pair.public_key
+
+	def get_private_key(self):
+		'''
+		Returns private key
+		'''
+		return self.pair.private_key
+
---
+++ ntl_tester.py
@@ -0,0 +1,57 @@
+'''
+	#############################
+	NetworkTransportLayer Tester v2
+	#############################
+	
+	Written by Henry Harder
+	for Paradigm, 2018
+
+	@version 0.0.3
+	@version_date: 9 April 2018
+
+	https://paradigm.market/
+	https://paradigmfoundation.io/
+'''
+
+# import NetworkBroadcastLayer toolkit
+from ntl import*
+from time import time
+import pyperclip as pyp
+
+'''
+#########
+this is PRIVATE 
+#########
+'''
+app_id = 'fd3fc431'
+app_key = 'f0ecbe406567c7f05507345643bb8e55'
+
+
+'''
+#########
+END PRIVATE DATA 
+#########
+'''
+
+
+order = {'data':{'message':'SHOULDA BEEN POST'}}
+metadata = {'metadata':{'timestamp':time()}}
+
+# generate a random test keypair
+# in reality, real keys would be used
+alice = KeypairGenerator()
+
+# create instance of NTL
+broadcaster = NetworkTransportLayer(app_id, app_key)
+
+# create a transaction
+test_tx = broadcaster.make_singlesign_order(
+	order, metadata, alice.get_public_key()) 
+
+signed_tx = broadcaster.sign_order(test_tx, alice.get_private_key())
+
+sent_order = broadcaster.send_order(signed_tx)
+
+print(sent_order[0])
+pyp.copy(sent_order[0])
+
